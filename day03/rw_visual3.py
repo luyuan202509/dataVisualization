c02846167b8feeb0747a13d7d819e8162df901fe
luyuan <luyuan202509@gmail.com>
2025-09-07 20:50:58 +0800

    隐藏坐标轴

--- day03/rw_visual3.py
+++ day03/rw_visual3.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt 
 from random_walk import RandomWalk
-'''隐藏坐标隐藏坐标轴'''
+'''隐藏坐标轴'''
 
 while True:
     rw = RandomWalk()
@@ -12,6 +12,14 @@ while True:
     # 突出起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100) 
     plt.scatter(rw.x_values[-1],rw.y_values[-1],c = 'red',edgecolors= 'none',s = 100)
+    
+    #隐藏坐标轴
+    ax = plt.gca()
+    ax.set_axis_off()
+    # 或者,这里会有问题
+    #ax.xaxis.set_visible(False)
+    #ax.yaxis.set_visible(False)
+
     plt.show()
 
     keep_running = input('make another walk?(y/n): ')
