9bb0878255a98bcd5614e9ed28d3469a5a522120
luyuan <luyuan202509@gmail.com>
2025-09-08 09:43:18 +0800

    生成数据结束

--- day04/die_visua_test03.py
+++ day04/die_visua_test03.py
@@ -1,22 +1,23 @@
 from die import Die
 import pygal
-'''同时掷2个骰子'''
+'''同时掷3个骰子'''
 from pathlib import Path
 # 保存到当前脚本所在目录的 image 子目录（/dataVisualization/day04/image）
 image_dir = Path(__file__).resolve().parent / "image"
 image_dir.mkdir(parents=True, exist_ok=True)
 
 
-die6 = Die(8)
-die10 = Die(8)
+die1 = Die()
+die2 = Die()
+die3 = Die()
 results = []
-for row_num in range(int(5e800)):
-    s = die6.row() + die10.row()
+for row_num in range(int(50000)):
+    s = die1.row() + die2.row() + die3.row()
     results.append(s)
 
 # 分析结果
 frequencies = []
-max_size = die6.num_size + die10.num_size
+max_size = die1.num_size * 3
 for value in range(2,max_size +1):
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -25,12 +26,12 @@ print(frequencies)
 # 进行格式化
 hist = pygal.Bar()
 hist.title = "Results of rolling two D6 and D10 dice 500000 times"
-hist.x_lable = list(range(2,17,1))
+hist.x_lable = list(range(3,18,1))
 
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('d6 + d10',frequencies)
-svg_dir = image_dir / 'die_visual4.svg'
+hist.add('3 d6',frequencies)
+svg_dir = image_dir / 'die_visual5.svg'
 hist.render_to_file(svg_dir)
 
