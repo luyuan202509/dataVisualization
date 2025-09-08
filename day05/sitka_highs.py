15c7b7a7824f89738ef073337a0af73c7e718c4b
luyuan <luyuan202509@gmail.com>
2025-09-08 18:26:26 +0800

    添加x轴日期

--- day05/sitka_highs.py
+++ day05/sitka_highs.py
@@ -1,6 +1,7 @@
 import csv
 from pathlib import Path
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 scvPath = Path(__file__).resolve().parent / 'weather_data'
 
@@ -12,21 +13,24 @@ reader = csv.reader(lines)
 header_row  = next(reader)
 
 #提取最高温度
-highs = []
+dates,highs = [],[]
 '''
 for index,column_header in enumerate(header_row):
     print(index,column_header)
 '''
 for row in reader:
+    current_date = datetime.strptime(row[2],'%Y-%M-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 plt.style.use('default')
 fig,ax = plt.subplots()
-ax.plot(highs,color='red')
+ax.plot(dates,highs,color='red')
 
 ax.set_title("Daily High Temperatures, July 2021",fontsize = 14)
 ax.set_xlabel('Time',fontsize = 16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F",fontsize = 16)
 ax.minorticks_on()
 ax.tick_params(labelsize =8)
