6c10ee711a023e64722d22a4e12dff36c681de22
luyuan <luyuan202509@gmail.com>
2025-09-09 11:02:14 +0800

    json数据写入新文件，写入时美化

---
+++ unit06/eq_explore_data2.py
@@ -0,0 +1,14 @@
+import json 
+from pathlib import Path
+
+data_path = Path(__file__).resolve().parent / 'eq_data'
+file = data_path / 'eq_data_1_day_m1.geojson'
+
+contents =  file.read_text(encoding='utf-8')
+all_eq_datas = json.loads(contents)
+#print(all_eq_data)
+
+new_file = data_path / 'readable_eq_data_new.geojson'
+
+readble_contents =  json.dumps(all_eq_datas,indent=4)
+new_file.write_text(readble_contents)
