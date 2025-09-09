3fcad77d219d27e68a0c64f2c1705e0fb70c2a6f
luyuan <luyuan202509@gmail.com>
2025-09-09 09:52:33 +0800

    设置坐标轴的范围（x 轴和 y 轴的最小值和最大值)等价写法

--- day01/scatter_squares3.py
+++ day01/scatter_squares3.py
@@ -4,6 +4,8 @@ y_values = [x**2 for x in x_values]
 plt.scatter(x_values,y_values,s=40,edgecolors='none',c=(0, 0, 0.8))
 #plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
 
-plt.axis([0,1100,0,1100000])
+#plt.axis([0,1100,0,1100000])
+plt.xlim(0.1100)
+plt.ylim(1,1100000)
 plt.show()
 exit();
