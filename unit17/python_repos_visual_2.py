1ab56ea0be39e040526dc2f66a232339719861f5
luyuan <luyuan202509@gmail.com>
2025-09-10 14:55:22 +0800

    x轴添加可单击的链接

--- unit17/python_repos_visual_2.py
+++ unit17/python_repos_visual_2.py
@@ -1,6 +1,7 @@
 from plotly.graph_objs.layout import xaxis
 import requests as req
 import plotly.express as px
+''' 添加可单击的链接 '''
 
 #url = 'https://api.github.com/search/repositories?q=language:python+sort:stars'
 url = 'http://localhost:19990/region/gihub/repos'
@@ -13,11 +14,13 @@ repo_dicts = response_dict['items']
 print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # 处理仓库相关信息
-repo_names, stars,hover_texts = [], [],[]
+repo_links, stars,hover_texts = [], [],[]
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name  = repo_dict['name'],
+    repo_url = repo_dict['html_url'],
+    repo_links.append(f"<a href='{repo_url}'>{repo_name}</a>")
+    owner = repo_dict['owner']['login'],
     stars.append(float(repo_dict['stargazers_count']))
-    owner = repo_dict['owner']['login']
     description = repo_dict['description']
 
     hover_text = f"{owner}<br />{description}"
@@ -25,7 +28,7 @@ for repo_dict in repo_dicts:
 
 title = '"Most-Starred Python Projects on GitHub'
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x = repo_names,y = stars,title = title,labels = labels,hover_name= hover_texts)
+fig = px.bar(x = repo_links,y = stars,title = title,labels = labels,hover_name= hover_texts)
 fig.update_layout(title_x = 0.5,
                   title_font_size = 28, 
                   xaxis_title = 24,
